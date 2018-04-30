de2cb7f3dfd1135b4f39bae66bd3695eb07fa8ca
Erlon F Souza <erlon.efs@gmail.com>
2018-04-30 15:07:33 -0300

    Add Swagger

---
+++ Competencia.Api/Controllers/CompetenciaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Competencia.Api.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/competencia")]
+	public class CompetenciaController : Controller
+	{
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		[HttpGet("{id:guid}", Name = "Get")]
+		public string Get(Guid id)
+		{
+			return "value";
+		}
+
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		[HttpPut("{id:guid}")]
+		public void Put(Guid id, [FromBody]string value)
+		{
+		}
+
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
--- Competencia.Api/Startup.cs
+++ Competencia.Api/Startup.cs
@@ -8,33 +8,45 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Competencia.Api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "Competencia API", Version = "v1" });
+			});
+		}
 
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Competencia API V1");
+			});
+
+		}
+	}
 }
